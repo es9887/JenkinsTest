4725f043554e7154f925b1d24d8279dc1f69150c
es9887 <es9887@hotmail.com>
2016-12-08 09:30:10 +0800

    20161208 0930

--- HelloJUnit/src/main/java/Subscriber.java
+++ HelloJUnit/src/main/java/Subscriber.java
@@ -1,14 +1,18 @@
 
 public class Subscriber {
 	public boolean deleteSubscriber(String msisdn) {
-		switch (msisdn) {
-		case "09123456789":
-			return false;
-		case "0911111111":
+		// switch (msisdn) {
+		// case "09123456789":
+		// return false;
+		// case "0911111111":
+		// return true;
+		// default:
+		// return false;
+		// }
+		if (msisdn == "0911111111") {
 			return true;
-		default:
+		} else {
 			return false;
 		}
-
 	}
 }
