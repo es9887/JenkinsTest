bd3926b5dfd2cf3685ee7595f651f7875447f9e2
es9887 <es9887@hotmail.com>
2016-12-13 17:36:34 +0800

    style

--- HelloJUnit/src/main/java/test/Subscriber.java
+++ HelloJUnit/src/main/java/test/Subscriber.java
@@ -2,7 +2,7 @@ package test;
 
 public class Subscriber {
 	
-	private String test;
+	private String Test;
 	public boolean deleteSubscriber(String msisdn) {
 		// switch (msisdn) {
 		// case "09123456789":
